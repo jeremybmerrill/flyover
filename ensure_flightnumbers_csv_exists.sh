0efe25fcd8e22c357b9993c658d558ce0bb3ded8
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2021-07-20 11:17:04 -0400

    depnd on server timestamp to not duplicate flightnumbers.csv

--- ensure_flightnumbers_csv_exists.sh
+++ ensure_flightnumbers_csv_exists.sh
@@ -5,7 +5,7 @@ pushd $(dirname $0)
 if [ -f FlightNumbers.csv ]; then
   mv FlightNumbers.csv FlightNumbers.csv.bak
 fi
-wget -q -O FlightNumbers.csv -nc http://www.virtualradarserver.co.uk/Files/FlightNumbers.csv
+wget -N  http://www.virtualradarserver.co.uk/Files/FlightNumbers.csv
 rm -rf flightnumbers.sqlite3
 sqlite3 flightnumbers.sqlite3 -cmd ".mode csv" ".import FlightNumbers.csv flightnumbers"
 popd
