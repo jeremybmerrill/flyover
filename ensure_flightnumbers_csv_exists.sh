093d13c88a51030474ec1eadbf0e26c9b1075fcd
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2021-07-21 08:59:32 -0400

    cope with BOM in FlightNumbers.csv ugh

--- ensure_flightnumbers_csv_exists.sh
+++ ensure_flightnumbers_csv_exists.sh
@@ -6,6 +6,7 @@ if [ -f FlightNumbers.csv ]; then
   mv FlightNumbers.csv FlightNumbers.csv.bak
 fi
 wget -N  http://www.virtualradarserver.co.uk/Files/FlightNumbers.csv
+sed '1s/^\xEF\xBB\xBF//' < FlightNumbers.csv > FlightNumbers.nobom.csv
 rm -rf flightnumbers.sqlite3
-sqlite3 flightnumbers.sqlite3 -cmd ".mode csv" ".import FlightNumbers.csv flightnumbers"
+sqlite3 flightnumbers.sqlite3 -cmd ".mode csv" ".import FlightNumbers.nobom.csv flightnumbers"
 popd
